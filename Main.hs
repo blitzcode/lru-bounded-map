9b96d009db8b4e6b3036549d03b6b2e1c0c6019d
Tim C. Schroeder <tim@blitzcode.net>
2013-11-27 12:21:53 +0100

    Shortened benchmark / group names

--- Main.hs
+++ Main.hs
@@ -9,7 +9,6 @@ import Data.List
 -- import Data.Function
 import Data.Maybe
 import Data.Hashable
-import Text.Printf
 import Control.Applicative
 import Control.Monad
 import Control.DeepSeq
@@ -56,8 +55,7 @@ main = do
         void $ printf "Collision(s) found: %s\n" $ show collisions
         exitFailure
     -}
-    let numK = printf "(%ik keys)" $ length keysL `div` 1000
-        kvL  = zip keysL ([1..] :: [Int])
+    let kvL  = zip keysL ([1..] :: [Int])
         -- Make initial maps for delete / lookup benchmarks
         mkDMS         = foldl' (\r (k, v) ->       M.insert k v r)         (M.empty)
         mkDHMS        = foldl' (\r (k, v) ->       HM.insert k v r)        (HM.empty)
@@ -153,25 +151,25 @@ main = do
         criterionCfg
         (return ())
         [
-          bgroup ("insert (w/o LRU update)" ++ numK)
+          bgroup "insert (w/o LRU upd)"
           [
             bench "Data.Map.Strict"                    . nf (mkDMS)         $ kvL
           , bench "Data.HashMap.Strict"                . nf (mkDHMS)        $ kvL
           , bench "Data.IntMap.Strict"                 . nf (mkDIMS)        $ kvL
           , bench "LBM_CustomHAMT"                     . nf (mkLBM_CHAMT5k) $ kvL
           ]
-        , bgroup ("insert (w/ LRU update)" ++ numK)
+        , bgroup "insert (w/ LRU upd)"
           [
           {- -- Disabled, too slow (uses O(n) size call)
-          , bench "LBM_LinkedListHashMap (limit 5k)"   . nf (mkLBM_LLHM5k)  $ kvL
-          , bench "LBM_LinkedListHashMap (limit 1k)"   . nf (mkLBM_LLHM1k)  $ kvL
+          , bench "LBM_LinkedListHM (lim 5k)"        . nf (mkLBM_LLHM5k)  $ kvL
+          , bench "LBM_LinkedListHM (lim 1k)"        . nf (mkLBM_LLHM1k)  $ kvL
           -}
-            bench "LBM_DoubleMapBTree (limit 5k)"      . nf (mkLBM_DMBT5k)  $ kvL
-          , bench "LBM_DoubleMapBTree (limit 1k)"      . nf (mkLBM_DMBT1k)  $ kvL
-          , bench "LBM_CustomHashedTrie (limit 5k)"    . nf (mkLBM_CHT5k)   $ kvL
-          , bench "LBM_CustomHashedTrie (limit 1k)"    . nf (mkLBM_CHT1k)   $ kvL
+            bench "LBM_DoubleMapBTree (lim 5k)"      . nf (mkLBM_DMBT5k)  $ kvL
+          , bench "LBM_DoubleMapBTree (lim 1k)"      . nf (mkLBM_DMBT1k)  $ kvL
+          , bench "LBM_CustomHashedTrie (lim 5k)"    . nf (mkLBM_CHT5k)   $ kvL
+          , bench "LBM_CustomHashedTrie (lim 1k)"    . nf (mkLBM_CHT1k)   $ kvL
           ]
-        , bgroup ("delete (w/o LRU update)" ++ numK)
+        , bgroup "delete (w/o LRU upd)"
           [
             bench "Data.Map.Strict" . nf
               (foldl' (\r k -> M.delete k r) lDMS) $ keysLShuffled
@@ -182,22 +180,22 @@ main = do
           , bench "LBM_CustomHAMT" . nf
               (foldl' (\r k -> fst $ LBM_CHAMT.delete k r) lLBM_CHAMT5k) $ keysLShuffled
           ]
-        , bgroup ("delete (w/ LRU update)" ++ numK)
+        , bgroup "delete (w/ LRU upd)"
           [
-            bench "LBM_LinkedListHashMap (limit 5k)" . nf
+            bench "LBM_LinkedListHM (lim 5k)" . nf
               (foldl' (\r k -> fst $ LBM_LLHM.delete k r) lLBM_LLHM5k) $ keysLShuffled
-          , bench "LBM_LinkedListHashMap (limit 1k)" . nf
+          , bench "LBM_LinkedListHM (lim 1k)" . nf
               (foldl' (\r k -> fst $ LBM_LLHM.delete k r) lLBM_LLHM1k) $ keysLShuffled
-          , bench "LBM_DoubleMapBTree (limit 5k)" . nf
+          , bench "LBM_DoubleMapBTree (lim 5k)" . nf
               (foldl' (\r k -> LBM_DMBT.delete k r) lLBM_DMBT5k) $ keysLShuffled
-          , bench "LBM_DoubleMapBTree (limit 1k)" . nf
+          , bench "LBM_DoubleMapBTree (lim 1k)" . nf
               (foldl' (\r k -> LBM_DMBT.delete k r) lLBM_DMBT1k) $ keysLShuffled
-          , bench "LBM_CustomHashedTrie (limit 5k)" . nf
+          , bench "LBM_CustomHashedTrie (lim 5k)" . nf
               (foldl' (\r k -> fst $ LBM_CHT.delete k r) lLBM_CHT5k) $ keysLShuffled
-          , bench "LBM_CustomHashedTrie (limit 1k)" . nf
+          , bench "LBM_CustomHashedTrie (lim 1k)" . nf
               (foldl' (\r k -> fst $ LBM_CHT.delete k r) lLBM_CHT1k) $ keysLShuffled
           ]
-        , bgroup ("lookup (w/o LRU update) " ++ numK)
+        , bgroup "lookup (w/o LRU upd) "
           [
             bench "Data.Map.Strict" . nf
               (foldl' (\r k -> (r +) . fromJust . M.lookup k $ lDMS) 0) $ keysLShuffled
@@ -205,10 +203,10 @@ main = do
               (foldl' (\r k -> (r +) . fromJust . HM.lookup k $ lDHMS) 0) $ keysLShuffled
           , bench "Data.IntMap.Strict" . nf
               (foldl' (\r k -> (r +) . fromJust . IM.lookup (hash k) $ lDIMS) 0) $ keysLShuffled
-          , bench "LBM_CustomHashedTrie (limit 5k)" . nf
+          , bench "LBM_CustomHashedTrie (lim 5k)" . nf
               (foldl' (\r k -> (r +)
                   . fromMaybe 0 . LBM_CHT.lookupNoLRU k $ lLBM_CHT5k) 0) $ keysLShuffled
-          , bench "LBM_CustomHashedTrie (limit 1k)" . nf
+          , bench "LBM_CustomHashedTrie (lim 1k)" . nf
               (foldl' (\r k -> (r +)
                   . fromMaybe 0 . LBM_CHT.lookupNoLRU k $ lLBM_CHT1k) 0) $ keysLShuffled
           , bench "LBM_CustomHAMT" . nf
@@ -219,21 +217,21 @@ main = do
           -- special case with the LLHM where the key to update is always at the end /
           -- front of the LRU linked-list, hence making it appear to perform better than
           -- it likely will in a real-word situation
-        , bgroup ("lookup (w/ LRU update, ordered keys) " ++ numK)
+        , bgroup "lookup (w/ LRU upd, ord keys) "
           [
-            lkBench "LBM_LinkedListHashMap (limit 5k)" (LBM_LLHM.lookup)  lLBM_LLHM5k keysL
-          , lkBench "LBM_LinkedListHashMap (limit 1k)" (LBM_LLHM.lookup)  lLBM_LLHM1k keysL
+            lkBench "LBM_LinkedListHM (lim 5k)" (LBM_LLHM.lookup)  lLBM_LLHM5k keysL
+          , lkBench "LBM_LinkedListHM (lim 1k)" (LBM_LLHM.lookup)  lLBM_LLHM1k keysL
           ]
-        , bgroup ("lookup (w/ LRU update) " ++ numK)
+        , bgroup "lookup (w/ LRU upd) "
           [
-            lkBench "LBM_LinkedListHashMap (limit 5k)" (LBM_LLHM.lookup)  lLBM_LLHM5k keysLShuffled
-          , lkBench "LBM_LinkedListHashMap (limit 1k)" (LBM_LLHM.lookup)  lLBM_LLHM1k keysLShuffled
-          , lkBench "LBM_DoubleMapBTree (limit 5k)"    (LBM_DMBT.lookup)  lLBM_DMBT5k keysLShuffled
-          , lkBench "LBM_DoubleMapBTree (limit 1k)"    (LBM_DMBT.lookup)  lLBM_DMBT1k keysLShuffled
-        --, lkBench "LBM_CustomHAMT (limit 5k)"        (LBM_CHAMT.lookup) lLBM_CHAMT5k keysLShuffled
-        --, lkBench "LBM_CustomHAMT (limit 1k)"        (LBM_CHAMT.lookup) lLBM_CHAMT1k keysLShuffled
-          , lkBench "LBM_CustomHashedTrie (limit 5k)"  (LBM_CHT.lookup)   lLBM_CHT5k  keysLShuffled
-          , lkBench "LBM_CustomHashedTrie (limit 1k)"  (LBM_CHT.lookup)   lLBM_CHT1k  keysLShuffled
+            lkBench "LBM_LinkedListHM (lim 5k)"      (LBM_LLHM.lookup)  lLBM_LLHM5k keysLShuffled
+          , lkBench "LBM_LinkedListHM (lim 1k)"      (LBM_LLHM.lookup)  lLBM_LLHM1k keysLShuffled
+          , lkBench "LBM_DoubleMapBTree (lim 5k)"    (LBM_DMBT.lookup)  lLBM_DMBT5k keysLShuffled
+          , lkBench "LBM_DoubleMapBTree (lim 1k)"    (LBM_DMBT.lookup)  lLBM_DMBT1k keysLShuffled
+        --, lkBench "LBM_CustomHAMT (lim 5k)"        (LBM_CHAMT.lookup) lLBM_CHAMT5k keysLShuffled
+        --, lkBench "LBM_CustomHAMT (lim 1k)"        (LBM_CHAMT.lookup) lLBM_CHAMT1k keysLShuffled
+          , lkBench "LBM_CustomHashedTrie (lim 5k)"  (LBM_CHT.lookup)   lLBM_CHT5k  keysLShuffled
+          , lkBench "LBM_CustomHashedTrie (lim 1k)"  (LBM_CHT.lookup)   lLBM_CHT1k  keysLShuffled
           ]
         ]
     where {-# INLINE lkBench #-}
